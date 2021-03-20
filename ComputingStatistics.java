f383d49d496a31ff56ce942dc64c69aab2bb9367
Jonathan Hsing <600020173@fjuhsd.org>
2021-03-20 19:14:26 +0000

    imported part 1

--- ComputingStatistics.java
+++ ComputingStatistics.java
@@ -31,4 +31,159 @@ public class ComputingStatistics {
       }
       return amount;
    }
+   public double avgLoan(){
+     double average = this.totalAmount() / data.size();
+     return average;
+   }
+
+   //statsitics
+  
+public double largestLoan(){
+  //create int = 0
+  double large = 0;
+  //for each loop
+  for(Loan x: data){
+    //if loan at x is greater than int
+    if(x.getLoanAmount() > large){
+      //set equal
+      large = x.getLoanAmount();
+    }
+  }
+
+  //return large
+  return large;
+}
+
+public double smallestLoan(){
+  //create int as first loan
+  double small = data.get(0).getLoanAmount();
+  //for each loop
+  for(Loan x: data){
+    //if loan at x is less than
+    if(x.getLoanAmount() < small){
+      small = x.getLoanAmount();
+    }
+  }
+
+  //return small
+  return small;
+}
+
+public String largestLoanCountry(){
+  //create int 
+  int index = 0;
+  //create double
+  double largest = 0;
+  //for loop
+  for(int x = 0; x < data.size(); x++){
+    //if value at x is greater than double
+    if(data.get(x).getLoanAmount() > largest){
+      //set value at x for double
+      largest = data.get(x).getLoanAmount();
+      //set int at x
+      index = x;
+    }
+  }
+
+  //set String country as country at data index
+  String country = data.get(index).getCountry();
+
+  //return country
+  return country;
+}
+
+public String smallestLoanCountry(){
+  //create int variable index
+  int index = 0;
+  //create int variable smallest
+  double smallest = data.get(0).getLoanAmount();
+  //for loop
+  for(int x = 0; x < data.size(); x++){
+    //if value at x is lesser than smallest
+    if(data.get(x).getLoanAmount() < smallest){
+      //set value at x for smallest
+      smallest = data.get(x).getLoanAmount();
+      //set index at x
+      index = x;
+    }
+  }
+
+  //set String country as country at data index
+  String country = data.get(index).getCountry();
+
+  //return country
+  return country;
+}
+
+public double avgDaysToFund(){
+  //create double total
+  double total = 0;
+  //for each loop 
+  for(Loan x: data){
+    //add days to total
+    total = total + x.getDaysToFund();
+  }
+
+  //return average
+  return total/data.size();
+}
+
+public double largestLoanKenya(){
+  //create double largest
+  double largest = 0;
+
+  //for each loop 
+  for(Loan x: data){
+    //if country at x is Kenya
+    if(x.getCountry().equals("Kenya")){
+      //if loan at x is larger than largest
+      if(x.getLoanAmount() > largest){
+        //set largest to loan
+        largest = x.getLoanAmount();
+      }
+
+    }
+  }
+
+  //return largest
+  return largest;
+}
+
+public double avgLoanPhilippines(){
+  double total = 0;
+  int loanCount = 0;
+  for(Loan i:data){
+    if(i.getCountry().equals("Philippines")){
+      total += i.getLoanAmount();
+      loanCount++;
+    }
+  }
+  if(loanCount == 0){
+    return 0;
+  }
+  return (double)total/loanCount;
+}
+
+public String longestToFundCountry(){
+  int longest = Integer.MIN_VALUE;
+  Loan longestLoan = data.get(0);
+  for(Loan i:data){
+    if(i.getDaysToFund() > longest){
+      longest = i.getDaysToFund();
+      longestLoan = i;
+    }
+  }
+  return longestLoan.getCountry();
+}
+
+public double variance(){
+  double sum = 0.0;
+  for(Loan i:data){
+    sum += Math.pow(i.getLoanAmount() - this.avgLoan(), 2);
+  }
+  return (double)sum/data.size(); 
+  }
+  public double standardDeviation(){
+    return Math.sqrt(this.variance());
+  }
 }
